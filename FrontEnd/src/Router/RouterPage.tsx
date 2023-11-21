e6ef228c5106e693d18096cfa20e7927e2b99915
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2023-11-21 18:58:00 +0200

    HomePage-Front

--- FrontEnd/src/Router/RouterPage.tsx
+++ FrontEnd/src/Router/RouterPage.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import App from "../App";
+import Deshboad from "../Vies/deshboad";
+import HomePage from "../Vies/HomePage";
+import Login from "../Vies/login";
+import Reg from "../Vies/reg";
 
 const RouterPage = () => {
-  return (
-    <div>RouterPage</div>
+  const routerMain = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<App/>}>
+        <Route index element={<  HomePage />}/>
+        <Route path="login" element={<Login />} />
+        <Route path="dashboard" element={<Deshboad />}>
+          <Route path="reg" element={<Reg />} />
+        </Route>
+      </Route>
+    )
   )
+  return <RouterProvider router={routerMain}/>
 }
 
 export default RouterPage
